f1f6d3e7582ce71a70c32344541720a8eefa34a0
venkatprasath <venkatprasath.perumal@listertechnologies.com>
2014-03-13 17:46:10 +0530

    comment added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  #the current cart comment is added to make things look good
   private 
   	def current_cart
   		Cart.find(session[:cart_id])
